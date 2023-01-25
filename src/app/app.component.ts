3b1a93d1b085eba78d41b02b983dd0a408e1d5aa
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-01-25 21:04:53 +0300

    5 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component, VERSION } from '@angular/core';
 
+export type Role = 'MANAGER' | 'WRITER' | 'READER' | 'CLIENT';
+
+export interface User {
+  name: string;
+  isAdmin: boolean;
+  roles: Role[];
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
