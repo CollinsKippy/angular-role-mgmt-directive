5695e716425b1fef1d03860f19e7bfb642966f1a
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-01-26 12:05:42 +0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatCardModule } from '@angular/material/card';
 import { RoleService } from './role.service';
+import { HasRoleDirective } from './has-role.directive';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatCardModule, NavbarComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    NavbarComponent,
+    HasRoleDirective,
+  ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
   providers: [RoleService],
--- src/app/has-role.directive.ts
+++ src/app/has-role.directive.ts
@@ -1,13 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, Input } from '@angular/core';
+import { Role } from './app.component';
 import { RoleService } from './role.service';
 
 @Directive({
   selector: '[hasRole]',
+  standalone: true,
 })
 export class HasRoleDirective {
-  constructor(private roleService: RoleService, private templateRef: TemplateRef<unknown>) {}
+  constructor(
+    private roleService: RoleService,
+    private templateRef: TemplateRef<unknown>
+  ) {}
 
-  @Input() set hasRole(roles: Role[]) {
-
-  }
+  @Input() set hasRole(roles: Role[]) {}
 }
