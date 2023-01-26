b4d7660b67354e5e477662ef3af30cdc721d632a
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-01-26 08:14:22 +0300

    6 files modified - buttons and fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 
-export type Role = 'MANAGER' | 'WRITER' | 'READER' | 'CLIENT';
+export type Role = 'ADMIN' | 'MANAGER' | 'WRITER' | 'READER' | 'CLIENT';
 
 export interface User {
   name: string;
@@ -15,4 +15,18 @@ export interface User {
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  buttonList: { role: Role; name: string }[] = [
+    { name: 'Admin', role: 'ADMIN' },
+    { name: 'Manager', role: 'MANAGER' },
+    { name: 'Reader', role: 'READER' },
+    { name: 'Writer', role: 'WRITER' },
+    { name: 'Reader and Writer', role: 'READER' || 'WRITER' },
+    { name: 'Client', role: 'CLIENT' },
+    { name: 'Public', role: undefined },
+  ];
+
+  onSelectRole(role: Role) {
+    console.log(role);
+  }
 }
